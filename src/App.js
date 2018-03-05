0ff2e8f9a8d1b90c99695a5d9367135885236081
Chai Phonbopit <phonbopit@gmail.com>
2018-03-05 11:10:01 +0700

    add basic example of i18next

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import React, { Component } from 'react'
+import React from 'react'
 import './App.css'
+import i18n from './i18n'
 
 import { translate } from 'react-i18next'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <button onClick={() => { changeLanguage('th') }}>TH</button>
-          <button onClick={() => { changeLanguage('en') }}>EN</button>
-        </header>
+const App = ({ t }) => (
+  <div className="App">
+    <header className="App-header">
+      <button onClick={() => { i18n.changeLanguage('th') }}>TH</button>
+      <button onClick={() => { i18n.changeLanguage('en') }}>EN</button>
+    </header>
 
-        <div className="content">
-          <h1>{t('title')}</h1>
-          <p>{t('content')}</p>
-        </div>
-      </div>
-    )
-  }
-}
+    <div className="content">
+      <h1>{t('title')}</h1>
+      <p>{t('content')}</p>
+    </div>
+  </div>
+)
 
 export default translate('trans')(App)
--- src/i18n.js
+++ src/i18n.js
@@ -1,9 +1,9 @@
 import i18n from 'i18next'
-import LanguageDetector from 'i18next-browser-languagedetector'
+import XHR from 'i18next-xhr-backend'
 import { reactI18nextModule } from 'react-i18next'
 
 i18n
-  .use(LanguageDetector)
+  .use(XHR)
   .use(reactI18nextModule)
   .init({
   	fallbackLng: 'th',
